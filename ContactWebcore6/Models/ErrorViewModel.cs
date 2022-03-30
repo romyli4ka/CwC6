90890bba7a1404bdcc7449c5be15314d4f66f5ee
unknown <romyli4ka@gmail.com>
2022-03-30 08:23:20 -0400

    init comit

---
+++ ContactWebcore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactWebcore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
